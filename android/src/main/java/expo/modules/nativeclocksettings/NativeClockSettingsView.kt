3c14d1801137b01395871bd51992cc0d842779e1
Yoann <yoann.le-hir@orange.fr>
2024-09-30 22:37:30 +0100

    first deploy function

--- android/src/main/java/expo/modules/nativeclocksettings/NativeClockSettingsView.kt
+++
@@ -1,7 +0,0 @@
-package expo.modules.nativeclocksettings
-
-import android.content.Context
-import expo.modules.kotlin.AppContext
-import expo.modules.kotlin.views.ExpoView
-
-class NativeClockSettingsView(context: Context, appContext: AppContext) : ExpoView(context, appContext)
